ceddb3c17bb0f9d47631aefee3438a4f1700c48c
gaby-fav <gabyfavala@gmail.com>
2022-07-05 22:14:33 -0300

    configurado create para ingresar datos

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-#CONTROLADOR
-#En carpeta templates va la vista
+# CONTROLADOR
+# En carpeta templates va la vista
+from flask import Flask, request
+from flask import render_template, request
+from flaskext.mysql import MySQL
 
-from flask import Flask
-from flask import render_template
-
-#punto de entrada de la aplicación.- siempre es igual
+# punto de entrada de la aplicación.- siempre es igual
 app = Flask(__name__)
 
-
+# Configuracion de base de datos
 mysql = MySQL()
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -15,10 +15,20 @@ app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'sistema22072'
 mysql.init_app(app)
 
-#enrutamientos
+# enrutamientos
 @app.route('/')
 def index():
+    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'DATO DE PRUEBA 2', 'CORREOPRUEBA2@GMAIL.COM', 'FOTO2.JPG');"
+    conn= mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+
     return render_template('empleados/index.html')  #carga el template
+
+@app.route('/create')
+def create():
+    return render_template('empleados/create.html')
 
 if __name__=='__main__':
     app.run(debug=True)
