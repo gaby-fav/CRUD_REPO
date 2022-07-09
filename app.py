e66028247338010bcfad82100a52cacc1fce1a23
gaby-fav <gabyfavala@gmail.com>
2022-07-09 16:22:43 -0300

    storage routing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # CONTROLADOR
 # En carpeta templates va la vista
+
 from flask import Flask, request
 from flask import render_template, request
 from flaskext.mysql import MySQL
@@ -29,6 +30,21 @@ def index():
 @app.route('/create')
 def create():
     return render_template('empleados/create.html')
+
+@app.route("/store", methods=['POST'])
+def storage():
+    _nombre = request.form ['txtNombre']
+    _correo = request.form ['txtCorreo']
+    _foto = request.files ['txtFoto']
+
+    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
+    datos = (_nombre , _correo , _foto.filename)
+    conn= mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql,datos)
+    conn.commit()
+
+    return render_template('empleados/index.html')
 
 if __name__=='__main__':
     app.run(debug=True)
