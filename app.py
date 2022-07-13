859a2a76d4d4f484afc27ca8d3cb821ba3110e00
gaby-fav <gabyfavala@gmail.com>
2022-07-12 22:50:17 -0300

    views

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import Flask, redirect, request
 from flask import render_template, request
 from flaskext.mysql import MySQL
 from datetime import datetime
+import os
+from flask import send_from_directory
 
 # punto de entrada de la aplicación.- siempre es igual
 app = Flask(__name__)
@@ -17,7 +19,17 @@ app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'sistema22072'
 mysql.init_app(app)
 
+CARPETA = os.path.join('uploads')
+app.config['CARPETA'] = CARPETA
+
 # enrutamientos
+#para mostrar la foto en el html
+@app.route('/uploads/<nombreFoto>')
+def uploads(nombreFoto):
+    return send_from_directory(app.config['CARPETA'], nombreFoto)
+
+
+
 @app.route('/')
 def index():
     sql = "SELECT * FROM `empleados`;"
@@ -25,7 +37,6 @@ def index():
     cursor=conn.cursor()
     cursor.execute(sql) 
     empleados = cursor.fetchall() #trae todo y lo mete en una tupla
-    print(empleados)
     conn.commit()
 
     return render_template('empleados/index.html', empleados = empleados)  #carga el template
@@ -41,13 +52,24 @@ def update():
     datos=(_nombre, _correo, _id)
     conn= mysql.connect()
     cursor=conn.cursor()
+    now = datetime.now()
+    time = now.strftime('%Y%H%M%S')
+
+    if _foto.filename != '':
+        nuevoNombreFoto = time + _foto.filename
+        _foto.save("uploads/"+ nuevoNombreFoto)
+
+    #BORRAR FOTO ANTERIOR: Me traigo el nombre de la foto guardada y luego la borro
+    cursor.execute('SELECT foto FROM empleados WHERE id=%s', _id)
+    fila = cursor.fetchone()
+    os.remove(os.path.join(app.config['CARPETA'],fila[0]))
+    cursor.execute('UPDATE empleados SET foto=%s WHERE id=%s',(nuevoNombreFoto, _id))
+
     cursor.execute(sql,datos) 
     empleados = cursor.fetchall() #trae todo y lo mete en una tupla
     conn.commit()
 
     return redirect('/')
-
-
 
 
 @app.route('/create')
@@ -74,12 +96,16 @@ def storage():
     cursor.execute(sql,datos)
     conn.commit()
 
-    return render_template('empleados/index.html')
+    return redirect('/')
 
 @app.route("/delete/<int:id>")
 def delete(id):
     conn= mysql.connect()
     cursor=conn.cursor()
+    #busco nombre de la foto y la elimino de la carpeta uploads antes de borrar registro en bbdd
+    cursor.execute('SELECT foto FROM empleados WHERE id=%s', id)
+    fila = cursor.fetchone()
+    os.remove(os.path.join(app.config['CARPETA'],fila[0]))
     cursor.execute("DELETE FROM empleados WHERE id=%s" , (id)) 
     conn.commit()
 
